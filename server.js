692aba5c62cf11ed8aeb83bf5f8322168a1028c4
KingoBoiii <kingodk@outlook.dk>
2020-07-09 20:43:32 +0200

    Node server init

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-CSSConditionRule;
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.port || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+
+
